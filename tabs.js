7e972a9f8883cb6544c50d327a2f66b52ab15e7b
Elene Qaldani <eleneqaldani1@gmail.com>
2023-11-19 13:24:15 +0400

    finished everything other than the technology page and the js functionality

---
+++ tabs.js
@@ -0,0 +1,37 @@
+const tabList = document.querySelector('[role="tablist"]'); //the querySelector('[role="tablist"]'); instead of using ids is just to make sure that we use the correct accessibility roles because, if we don't, none of the code below will work
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener("keydown", changeTabFocus);
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", changeTabPanel);
+});
+
+let tabFocus = 0;
+function changeTabFocus(e) {
+  const keydownLeft = 37;
+  const keydownRight = 39;
+
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+    tabs[tabFocus].setAttribute("tabindex", -1);
+  }
+
+  if (e.keyCode === keydownRight) {
+    tabFocus++;
+    if (tabFocus >= tabs.length) {
+      tabFocus = 0;
+    }
+  }
+
+  if (e.keyCode === keydownLeft) {
+    tabFocus--;
+    if (tabFocus < 0) {
+      tabFocus = tabs.length - 1;
+    }
+  }
+
+  tabs[tabFocus].setAttribute("tabindex", 0);
+  tabs[tabFocus].focus();
+}
+
+function changeTabPanel(e) {}
